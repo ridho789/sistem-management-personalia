d44987b088a71665077e183a23a44da58de217b4
ridho789 <ridhoalfian808@gmail.com>
2023-10-03 09:13:38 +0700

    delete migration file rename tbl perusahaan column penanggung jawab

--- resources/views/backend/login/login.blade.php
+++ resources/views/backend/login/login.blade.php
@@ -14,7 +14,7 @@
 
 <body class="h-100">
     <div class="authincation h-100">
-        <div class="container-fluid h-100">
+        <div class="container-fluid h-100"> 
             <div class="row justify-content-center h-100 align-items-center">
                 <div class="col-md-6">
                     <div class="authincation-content">
