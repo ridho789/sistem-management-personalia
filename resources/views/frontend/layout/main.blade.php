59f6ca8ce437c9a9d5f8a0882124fe641c68ea9d
ridho789 <ridhoalfian808@gmail.com>
2023-12-27 09:34:52 +0700

    update href link logo to # in main view

--- resources/views/frontend/layout/main.blade.php
+++ resources/views/frontend/layout/main.blade.php
@@ -42,7 +42,7 @@
             Nav header start
         ***********************************-->
         <div class="nav-header">
-            <a href="/dashboard" class="brand-logo">
+            <a href="#" class="brand-logo">
                 <img class="logo-abbr" src="{{ asset('') }}asset/images/logo.png" alt="">
                 <img class="logo-compact" src="{{ asset('') }}asset/images/logo-text.png" alt="">
                 <!-- <img class="brand-title" src="{{ asset('') }}asset/images/logo-text.png" alt=""> -->
